41110c26af9e833b23482f180ab182e3be07d026
Shakib448 <shakiba448@gmail.com>
2020-09-14 10:31:25 +0600

    React Hot Onion:

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Banner.css";
+import bannerImg from "../Resources/bannerbackground.png";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div className="banner__middle">
+        <div className="col-md-2"></div>
+        <div className="col-md-8">
+          <h2>Best food waiting for your belly</h2>
+          <br />
+          <input
+            type="text"
+            placeholder="Search food items"
+            className="banner__input"
+          />
+          <input type="submit" value="Submit" className="banner__button" />
+        </div>
+        <div className="col-md-2"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import navLogo from "../Resources/logo2.png";
+import "./Navbar.css";
+import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <img src={navLogo} alt="" className="navbar__logo" />
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                <ShoppingCartOutlinedIcon className="nav__cart" />
+              </a>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                <button className="nav__button">Login</button>
+              </a>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                <button className="nav__button__active">Sign In</button>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
