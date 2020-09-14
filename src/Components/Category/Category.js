89ff5ad6c6fb9290171b2cfa89ecea33c97d182f
Shakib448 <shakiba448@gmail.com>
2020-09-14 19:21:29 +0600

    React Hot Onion

--- src/Components/Category/Category.js
+++ src/Components/Category/Category.js
@@ -2,9 +2,6 @@ import React, { useEffect, useState } from "react";
 import CategoryDetail from "../CategoryDetail/CategoryDetail";
 import "./Category.css";
 import foodData from "../FoodData";
-import Breakfast from "../FoodData/Breakfast";
-import Lunch from "../FoodData/Lunch";
-import Dinner from "../FoodData/Dinner";
 
 const Category = () => {
   const [food, setFood] = useState("Lunch");
@@ -29,7 +26,10 @@ const Category = () => {
         <button className="category__btn" onClick={() => setFood("Breakfast")}>
           Breakfast
         </button>
-        <button className="category__btn" onClick={() => setFood("Lunch")}>
+        <button
+          className={`category__btn ${food && "category__active"}`}
+          onClick={() => setFood("Lunch")}
+        >
           Lunch
         </button>
         <button className="category__btn" onClick={() => setFood("Dinner")}>
