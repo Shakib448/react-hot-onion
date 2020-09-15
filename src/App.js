0f633537f41732eb66dae8f9fabf7da0c6cadc85
Shakib448 <shakiba448@gmail.com>
2020-09-15 12:16:28 +0600

    React Red  Onion

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NotFound from "./Components/NotFound/NotFound";
 import CategoryDetail from "./Components/CategoryDetail/CategoryDetail";
 import Login from "./Components/Login/Login";
+import DeliveryDetails from "./Components/DeliveryDetails/DeliveryDetails";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/food/category/:id" component={CategoryDetail} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/delivery/details" component={DeliveryDetails} />
           <Route exact path="*" component={NotFound} />
         </Switch>
         <Footer />
---
+++ src/Components/DeliveryDetails/DeliveryDetails.js
@@ -0,0 +1,96 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import "./DeliveryDetails.css";
+
+const DeliveryDetails = () => {
+  const { register, errors, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+  return (
+    <div>
+      <div className="deliveryDetails container">
+        <div className="row">
+          <div className="col-md-6">
+            <h1 className="ml-4">Edit Deliver Details</h1>
+            <hr className="ml-4" />
+            <form onSubmit={handleSubmit(onSubmit)} className="m-4">
+              <div className="form-group" style={{ textAlign: "center" }}>
+                <input
+                  name="delivery"
+                  className="form-control"
+                  placeholder="Deliver to door"
+                  ref={register({ required: true })}
+                />{" "}
+                <br />
+                <span style={{ color: "red" }}>
+                  {errors.delivery && "Delivery is required"}
+                </span>
+              </div>
+              <div className="form-group" style={{ textAlign: "center" }}>
+                <input
+                  name="roadNo"
+                  placeholder="Road Number"
+                  className="form-control"
+                  ref={register({ required: true })}
+                />
+                <br />
+                <span style={{ color: "red" }}>
+                  {errors.roadNo && "Road number is required"}
+                </span>
+              </div>
+              <div className="form-group" style={{ textAlign: "center" }}>
+                <input
+                  name="floor"
+                  placeholder="Flat, suite or floor"
+                  className="form-control"
+                  ref={register({ required: true })}
+                />{" "}
+                <br />
+                <span style={{ color: "red" }}>
+                  {errors.floor && "Floor address is required"}
+                </span>
+              </div>
+              <div className="form-group" style={{ textAlign: "center" }}>
+                <input
+                  name="business"
+                  placeholder="Business Name"
+                  className="form-control"
+                  ref={register({ required: true })}
+                />
+                <br />
+                <span style={{ color: "red" }}>
+                  {errors.business && "Business is required"}
+                </span>
+              </div>
+              <div className="form-group" style={{ textAlign: "center" }}>
+                <textarea
+                  rows="3"
+                  name="address"
+                  placeholder="Add delivery instructions"
+                  className="form-control"
+                  ref={register({ required: true })}
+                />
+                <br />
+                <span style={{ color: "red" }}>
+                  {errors.address && "Delivery details is required"}
+                </span>
+              </div>
+              <div style={{ textAlign: "center" }}>
+                <input
+                  className="btn btn-primary"
+                  type="submit"
+                  value="Save & Continue"
+                />
+              </div>
+            </form>
+          </div>
+          <div className="col-md-1">
+            <br />
+          </div>
+          <div className="col-md-6"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DeliveryDetails;
