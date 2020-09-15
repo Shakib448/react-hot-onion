251e795482ec0f187f5ca11ba4391c83508a909a
Shakib448 <shakiba448@gmail.com>
2020-09-15 18:45:28 +0600

    React Red  Onion

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,17 +2,73 @@ import React, { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import "./Login.css";
 import logoImg from "../Resources/logo2.png";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "../../firebase.config";
+
+firebase.initializeApp(firebaseConfig);
 
 const Login = () => {
   const { register, errors, handleSubmit, watch } = useForm({});
-  const [signIn, setSingIn] = useState([]);
   const password = useRef({});
   password.current = watch("password", "");
 
-  console.log(signIn);
+  // const [signIn, setSingIn] = useState([]);
+
+  const [newUserInfo, setNewUserInfo] = useState(false);
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    fullName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    error: "",
+  });
+
+  // console.log(user);
 
   const onSubmit = (data) => {
-    setSingIn(data);
+    setUser(data);
+    if (newUserInfo && user.email && user.password) {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(user.email, user.password)
+        .then((res) => {
+          const newUser = { ...user };
+          newUser.error = "";
+          newUser.success = true;
+          setUser(newUser);
+          console.log(res);
+        })
+        .catch((error) => {
+          const newUser = { ...user };
+          newUser.error = error.message;
+          newUser.success = false;
+          setUser(newUser);
+        });
+    }
+
+    if (!newUserInfo && user.email && user.password) {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(user.email, user.password)
+        .then((res) => {
+          const newUser = { ...user };
+          newUser.error = "";
+          newUser.success = true;
+          setUser(newUser);
+          console.log(res);
+        })
+        .catch((error) => {
+          const newUser = { ...user };
+          newUser.error = error.message;
+          newUser.success = false;
+          setUser(newUser);
+        });
+    }
   };
 
   return (
@@ -23,24 +79,26 @@ const Login = () => {
           <img src={logoImg} className="img-fluid" alt="some" />
           <br />
           <form onSubmit={handleSubmit(onSubmit)} className="m-4">
-            <div className="form-group" style={{ textAlign: "center" }}>
-              <input
-                name="fullName"
-                className="form-control"
-                placeholder="Full Name"
-                ref={register({
-                  required: "Name required",
-                  minLength: {
-                    value: 5,
-                    message: "Name should be 5 characters",
-                  },
-                })}
-              />{" "}
-              <br />
-              <span style={{ color: "red" }}>
-                {errors.fullName && errors.fullName.message}
-              </span>
-            </div>
+            {newUserInfo && (
+              <div className="form-group" style={{ textAlign: "center" }}>
+                <input
+                  name="fullName"
+                  className="form-control"
+                  placeholder="Full Name"
+                  ref={register({
+                    required: "Name required",
+                    minLength: {
+                      value: 5,
+                      message: "Name should be 5 characters",
+                    },
+                  })}
+                />{" "}
+                <br />
+                <span style={{ color: "red" }}>
+                  {errors.fullName && errors.fullName.message}
+                </span>
+              </div>
+            )}
             <div className="form-group" style={{ textAlign: "center" }}>
               <input
                 name="email"
@@ -97,9 +155,23 @@ const Login = () => {
                 // disabled
                 className="btn btn-primary"
                 type="submit"
-                value="Sign in"
+                value={newUserInfo ? "Sign up" : "Sing In"}
               />
+              <br />
+              <span
+                className="mt-4"
+                style={{ color: "red", marginTop: "20px", cursor: "pointer" }}
+                onClick={() => setNewUserInfo(!newUserInfo)}
+              >
+                <h4>Already have a account?</h4>
+              </span>
             </div>
+            {user.success && (
+              <p style={{ color: "green" }}>
+                {" "}
+                User {newUserInfo ? "created" : "Logged In"} successfully{" "}
+              </p>
+            )}
           </form>
         </div>
         <div className="col-md-3"></div>
---
+++ src/firebase.config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBrQGlBZ3AdKsW8Xv7bulC0wuY0fBkZRxY",
+  authDomain: "react-hot-onion.firebaseapp.com",
+  databaseURL: "https://react-hot-onion.firebaseio.com",
+  projectId: "react-hot-onion",
+  storageBucket: "react-hot-onion.appspot.com",
+  messagingSenderId: "177246724641",
+  appId: "1:177246724641:web:1722cdd9f1657ecdab3960",
+  measurementId: "G-S9PCGNCL1F",
+};
+
+export default firebaseConfig;
